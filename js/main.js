83f88c658b37399e6c5f816100d01ff43f3238f6
Dang Quang <quangthat97@gmail.com>
2023-11-20 20:15:46 +0700

    first pus

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const faqContainer = document.querySelector('.faq-content');
+
+	faqContainer.addEventListener('click', (e) => {
+		const groupHeader = e.target.closest('.faq-group-header');
+
+		if (!groupHeader) return;
+
+		const group = groupHeader.parentElement;
+		const groupBody = group.querySelector('.faq-group-body');
+		const icon = groupHeader.querySelector('i');
+
+		// Toggle Icon
+		icon.classList.toggle('fa-plus');
+		icon.classList.toggle('fa-minus');
+
+		// Toggle visibillity of body
+		groupBody.classList.toggle('open');
+
+		// Closes other open FAQ bodies
+		const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+		otherGroups.forEach((otherGroup) => {
+			if (otherGroup !== group) {
+				const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+				const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+				otherGroupBody.classList.remove('open');
+				otherIcon.classList.remove('fa-minus');
+				otherIcon.classList.add('fa-plus');
+			}
+		});
+	});
+});
+
+// Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+	const menuBtn = document.querySelector('.menu-toggle');
+	const mobileMenu = document.querySelector('.mobile-menu');
+
+	menuBtn.addEventListener('click', () => {
+		mobileMenu.classList.toggle('active');
+	});
+});
